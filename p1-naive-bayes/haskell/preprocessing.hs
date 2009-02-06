1f0f06d1668f7b2087f3d081278e89c287cec610
Tristan Sloughter <tristan@kfgx61.(none)>
2009-02-06 14:59:50 -0600

    moved to parsec for preprocessing

--- p1-naive-bayes/haskell/preprocessing.hs
+++ p1-naive-bayes/haskell/preprocessing.hs
@@ -1,41 +1,110 @@
 import System.IO
 import Control.Monad
 import Data.List
+import Text.ParserCombinators.Parsec
 
 type AttributeName = String
 
-data Attribute = Real (AttributeName, Integer) 
+data Attribute = Real AttributeName 
                | TupleValues (AttributeName, [String])
                  deriving (Show)
 type Attributes = [Attribute]
 
-split delim = takeWhile (not . null) . unfoldr (Just . (second $ drop 1) . break (==delim))
+makeTupleList values =
+    let (Right [r]) = parse arffFileRows "(unknown)" (values++"\n")
+    in r
 
-update_attributes :: String -> Attributes -> Attributes
-update_attributes line attributes =
-    let attribute_list = split ' ' line in    
-        (TupleValues (attribute_list!!1, [attribute_list!!2])):attributes
+constructAttributeList attributes =
+    map (\x -> if x!!1 == "REAL"
+               then (Real (x!!0))
+               else (TupleValues ((x!!0), (makeTupleList (reverse (drop 1 (reverse (drop 1 (x!!1))))))))) attributes
 
-preprocess :: String -> IO Attributes
-preprocess datafile =
-    do
-      input <- openFile datafile ReadMode
-      output <- openFile "updated_file" WriteMode
-      attributes <- preprocess_loop input []
-      hClose input
-      hClose output
-      return attributes
-
-preprocess_loop :: Handle -> Attributes -> IO Attributes
-preprocess_loop input attributes = 
-    do ineof <- hIsEOF input
-       if ineof
-           then return attributes
-           else do line <- hGetLine input                       
-                   preprocess_loop input (update_attributes line attributes)
-                                                         
-main = 
+arffFileAttributes = endBy attribute eol
+attribute = 
     do 
-      attributes <- preprocess "test"
-      putStr (show (head attributes))
-      return ()
+      string "@ATTRIBUTE "  
+      attribute <- sepBy attr_cell (many1 (char ' '))
+      return [attribute!!0, attribute!!1]
+attr_cell = quotedCell <|> many (noneOf " \t\n\r")
+
+arffFileRows = endBy row eol  
+row = sepBy cell (char ',')
+cell = quotedCell <|> many (noneOf ",\n\r")
+                      
+quotedCell = 
+    do char '"'
+       content <- many quotedChar
+       char '"' <?> "quote at end of cell"
+       return content
+
+quotedChar =
+        noneOf "\""
+    <|> try (string "\"\"" >> return '"')
+
+eol =   try (string "\n\r")
+    <|> try (string "\r\n")
+    <|> string "\n"
+    <|> string "\r"
+    <?> "end of line"
+
+parseARFFAttributes :: String -> Either ParseError [[String]]
+parseARFFAttributes input = parse arffFileAttributes "(unknown)" input
+
+parseARFFRows :: String -> Either ParseError [[String]]
+parseARFFRows input = parse arffFileRows "(unknown)" input
+
+updateValues (Right values) attributes new_values =
+    new_values
+updateValues (Left values) attributes new_values =
+    new_values
+
+getNewValues inh list other attributes new_values = do 
+    ineof <- hIsEOF inh
+    if ineof
+            then 
+                -- update new values lits
+                return (parseARFFRows list)
+            else 
+              if length list == 1000
+              then do
+                -- update new values lits 
+                let list' = parseARFFRows list
+                getNewValues inh [] list' attributes (updateValues list' attributes new_values)
+              else do                     
+                inpStr <- hGetLine inh
+                getNewValues inh (list++inpStr++"\n") other attributes new_values
+
+getAttributes inh list other = do 
+    ineof <- hIsEOF inh
+    if ineof
+            then 
+                return (parseARFFAttributes list)
+            else 
+              if length list == 1000
+              then do                
+                getAttributes inh [] (parseARFFAttributes list)
+              else do                     
+                inpStr <- hGetLine inh
+                if inpStr == "@DATA"
+                  then 
+                    return (parseARFFAttributes list)
+                  else
+                    getAttributes inh (list++inpStr++"\n") other
+
+parseARFF location list = do
+  inh <- openFile location ReadMode
+  outh <- openFile "updated" WriteMode
+  (Right attributes) <- getAttributes inh list (parseARFFAttributes "")
+  putStrLn (show (constructAttributeList attributes))
+  results <- getNewValues inh list (parseARFFRows "") (constructAttributeList attributes) []
+  hClose inh
+  return (results)
+
+main =
+    do
+      results <- parseARFF "test" []
+      case results of 
+            Left e -> do putStrLn "Error parsing input:"
+                         print e
+            Right r -> mapM_ print r
+
